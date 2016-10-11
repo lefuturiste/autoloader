811a30daeca9d29a51b904d0c75c5d66484fbe07
Le_Futuriste <matthieu.bessat@outlook.com>
2016-10-11 08:29:31 +0200

    Change

--- autoloader.php
+++ autoloader.php
@@ -1,45 +1,26 @@
 <?php
 /**
- * Developed by: mbess
+ * Developed by: le_futuriste
  * Date: 21/09/2016
  * Time: 18:56
 
  * Class Autoloader
  *
- * Cette classe sert à inclure les bonnnes classe que quand on a besoin*;
- * pour appeler la classe utilisez :
- *      require 'autoloader.php';
- *      Autoloader::register();
+ * This class allows you to automatically include other classes in the file or it is used for example visit /example folder
  *
- * EXEMPLE
-
-//INDEX.php
-require 'autoloader.php';
-Autoloader::register();
-
-
-$test = new test();
-$test->call();
-//test.php
-class test {
-    public function call(){
-        echo 'Called Successfully !';
-    }
-}
-
  */
 class Autoloader{
 
     /**
-     * Enregistre notre autoloader
+     * register our autoloader
      */
     static function register(){
         spl_autoload_register(array(__CLASS__, 'autoload'));
     }
 
     /**
-     * Inclue le fichier correspondant à notre classe
-     * @param $class string Le nom de la classe à charger
+     * Include file to $class
+     * @param $class string name of class
      */
     static function autoload($class){
     	if ($class == "ReCaptcha\ReCaptcha") {
--- example.php
+++ example.php
@@ -1,3 +1,7 @@
 <?php
 require 'CLASS PATH';
+//initially register
 Autoloader::register(); 
+
+$class = new class;
+//don't need include class file for $class.
